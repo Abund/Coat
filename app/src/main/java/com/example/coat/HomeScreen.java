13c8e69a294d6ae23dae926f56cb54845043caa3
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2020-05-19 10:20:56 +0100

    Created the menu of the application
    created the menu leading to effective navigation to different sides of the application

--- app/src/main/java/com/example/coat/HomeScreen.java
+++ app/src/main/java/com/example/coat/HomeScreen.java
@@ -1,14 +1,306 @@
 package com.example.coat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class HomeScreen extends AppCompatActivity {
+import com.example.coat.fragments.BlogPost;
+import com.example.coat.fragments.ChatRooms;
+import com.example.coat.fragments.Chats;
+import com.example.coat.fragments.PersonalPage;
+import com.example.coat.fragments.ViewPsychologist;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeScreen extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener  {
+
+    private ImageView imageViewCalories,imageViewBloodPressure,imageViewBloodSugar,imageViewProfile,imageViewHomePageProfile,imageViewGo;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser userf;
+    DatabaseReference myRef;
+    boolean closeApp;
+    private int TAKE_IMAGE_CODE=10001;
+    private static  final  int PICK_IMAGE=1;
+    Uri imageuri;
+    private  static final String TAG="HomeScreenActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference=database.getReference("users");
+        databaseReference.keepSynced(true);
+        myRef = FirebaseDatabase.getInstance().getReference().child("users");
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        imageViewProfile = navigationView.getHeaderView(0).findViewById(R.id.imageViewProfile);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        firebaseAuth =FirebaseAuth.getInstance();
+        userf=firebaseAuth.getCurrentUser();
+
+        ListView listView = new ListView(this);
+        List<String> data = new ArrayList<>();
+        data.add("Camera");
+        data.add("Gallery");
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
+        listView.setAdapter(adapter);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomeScreen.this);
+        builder.setCancelable(true);
+        builder.setView(listView);
+        final AlertDialog dialog=builder.create();
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user!=null){
+            if(user.getPhotoUrl()!=null){
+//                Glide.with(this)
+//                        .load(user.getPhotoUrl())
+//                        .into(imageViewProfile);
+//                Glide.with(this)
+//                        .load(user.getPhotoUrl())
+//                        .into(imageViewHomePageProfile);
+                Picasso.get().load(user.getPhotoUrl()).networkPolicy(NetworkPolicy.OFFLINE).into(imageViewProfile, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Picasso.get().load(user.getPhotoUrl()).into(imageViewProfile);
+                    }
+                });
+
+                Picasso.get().load(user.getPhotoUrl()).networkPolicy(NetworkPolicy.OFFLINE).into(imageViewHomePageProfile, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Picasso.get().load(user.getPhotoUrl()).into(imageViewHomePageProfile);
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==TAKE_IMAGE_CODE){
+            switch (resultCode){
+                case RESULT_OK:
+                    Bitmap bitmap =(Bitmap) data.getExtras().get("data");
+                    imageViewProfile.setImageBitmap(bitmap);
+                    handleUpload(bitmap);
+            }
+        }else if(requestCode==PICK_IMAGE){
+            //if(data.getData()!= null){
+
+            //}
+            switch (resultCode){
+                case RESULT_OK:
+                    Bitmap bitmap = null;
+                    try {
+                        imageuri = data.getData();
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imageuri);
+                        imageViewProfile.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    handleUpload(bitmap);
+            }
+        }
+    }
+
+    private void handleUpload(Bitmap bitmap){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos);
+
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final StorageReference reference = FirebaseStorage.getInstance().getReference()
+                .child("profileImages")
+                .child(uid+".jpeg");
+        reference.putBytes(baos.toByteArray())
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        getDownloadUrl(reference);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG,"onFailure",e.getCause());
+                    }
+                });
+    }
+
+    private void getDownloadUrl(StorageReference reference){
+        reference.getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Log.e(TAG,"onSuccess:"+uri);
+                        setUserProfile(uri);
+                    }
+                });
+    }
+
+    private void setUserProfile(Uri uri){
+        FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+        UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+                .setPhotoUri(uri).build();
+        user.updateProfile(request)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(HomeScreen.this,"Updated successfully",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(HomeScreen.this,"profile image failed",Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.homescreen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        //FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+            Intent at = new Intent(HomeScreen.this, HomeScreen.class);
+            startActivity(at);
+        } else if (id == R.id.nav_personalPage) {
+            Fragment newFragment =  new PersonalPage();
+//            FragmentTransaction ft = getFragmentManager().beginTransaction();
+//            ft.add(R.id.content_frame, newFragment).commit();
+            replaceFragment(newFragment);
+
+        }else if (id == R.id.nav_blogPost) {
+            Fragment newFragment =  new BlogPost();
+            replaceFragment(newFragment);
+
+        }else if (id == R.id.nav_chats) {
+            Fragment newFragment =  new Chats();
+            replaceFragment(newFragment);
+
+        } else if (id == R.id.nav_viewPsychologist) {
+            Fragment newFragment =  new ViewPsychologist();
+            replaceFragment(newFragment);
+
+        } else if (id == R.id.nav_chatRooms) {
+            Fragment newFragment =  new ChatRooms();
+            replaceFragment(newFragment);
+
+        }
+        else if (id == R.id.sign_out) {
+
+            firebaseAuth = FirebaseAuth.getInstance();
+            LoginManager.getInstance().logOut();
+            firebaseAuth.signOut();
+            Intent at = new Intent(HomeScreen.this, MainActivity.class);
+            startActivity(at);
+        }
+//
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void replaceFragment(Fragment destFragment)
+    {
+        // First get FragmentManager object.
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+
+        // Begin Fragment transaction.
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace the layout holder with the required Fragment object.
+        fragmentTransaction.replace(R.id.content_frame, destFragment);
+
+        // Commit the Fragment replace action.
+        fragmentTransaction.addToBackStack(null).commit();
     }
 }
---
+++ app/src/main/java/com/example/coat/fragments/ChatRooms.java
@@ -0,0 +1,30 @@
+package com.example.coat.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.coat.R;
+
+public class ChatRooms extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_chatrooms, container, false);
+        super.onCreate(savedInstanceState);
+        return view;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
