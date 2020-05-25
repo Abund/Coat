378b26a21461160f81291a8ff00eada32f709df0
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2020-05-25 08:12:46 +0100

    Created the personal page for the application
    created the personal page for the users to update there bio data

--- app/src/main/java/com/example/coat/fragments/PersonalPage.java
+++ app/src/main/java/com/example/coat/fragments/PersonalPage.java
@@ -1,25 +1,185 @@
 package com.example.coat.fragments;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.example.coat.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class PersonalPage extends Fragment {
 
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    ProgressDialog progressDialog;
+
+    private TextView firstNamePro,lastNamePro,addressPro,userNamePro,emailAddressPro;
+    FloatingActionButton floatingActionButton;
     View view;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_personalpage, container, false);
+
+        firebaseAuth= FirebaseAuth.getInstance();
+        user= firebaseAuth.getCurrentUser();
+        firebaseDatabase= FirebaseDatabase.getInstance();
+        databaseReference= firebaseDatabase.getReference("users");
+
+        firstNamePro = view.findViewById(R.id.firstNamePro);
+        lastNamePro = view.findViewById(R.id.lastNamePro);
+        addressPro = view.findViewById(R.id.addressPro);
+        userNamePro = view.findViewById(R.id.userNamePro);
+        emailAddressPro = view.findViewById(R.id.emailAddressPro);
+        floatingActionButton = view.findViewById(R.id.actionUpdate);
+
+        progressDialog = new ProgressDialog(getActivity());
+
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    String firstName =""+ ds.child("firstName").getValue();
+                    String lastName =""+ ds.child("lastName").getValue();
+                    String address =""+ ds.child("address").getValue();
+                    String UserName =""+ ds.child("userName").getValue();
+                    String email =""+ ds.child("email").getValue();
+
+                    firstNamePro.setText(firstName);
+                    lastNamePro.setText(lastName);
+                    addressPro.setText(address);
+                    userNamePro.setText(UserName);
+                    emailAddressPro.setText(email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                showEditProfileDialogue();
+            }
+        });
+
         super.onCreate(savedInstanceState);
         return view;
+    }
+
+    private void showEditProfileDialogue() {
+        String options[] = {"Edit first name","Edit last name","Edit address","Edit user name"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Choose Action");
+        builder.setCancelable(true);
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(i==0){
+                    progressDialog.setMessage("Updating first name");
+                    showFirstNameUpdateDialogue("firstName");
+
+                }else if(i==1){
+                    progressDialog.setMessage("Updating last name");
+                    showFirstNameUpdateDialogue("lastName");
+
+                }else if(i==2){
+                    progressDialog.setMessage("Updating address");
+                    showFirstNameUpdateDialogue("address");
+
+                }else if(i==3){
+                    progressDialog.setMessage("Updating user name");
+                    showFirstNameUpdateDialogue("userName");
+
+                }
+            }
+        });
+        builder.create().show();
+    }
+
+    private void showFirstNameUpdateDialogue(final String key) {
+
+        final AlertDialog.Builder builder= new AlertDialog.Builder(getActivity());
+        builder.setTitle("Update "+ key);
+        LinearLayout linearLayout = new LinearLayout(getActivity());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPadding(10,10,10,10);
+
+        final EditText editText= new EditText(getActivity());
+        editText.setHint("Enter "+key);
+        linearLayout.addView(editText);
+        builder.setView(linearLayout);
+        builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String value = editText.getText().toString().trim();
+
+                if(!TextUtils.isEmpty(value)){
+                    progressDialog.show();
+                    HashMap<String,Object> result = new HashMap<>();
+                    result.put(key,value);
+                    databaseReference.child(user.getUid()).updateChildren(result)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(getActivity(),"Updated... ",Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(getActivity()," "+e.getMessage(),Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }else{
+                    Toast.makeText(getActivity(),"Please Enter "+key+"",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.create().show();
     }
 
     @Override
