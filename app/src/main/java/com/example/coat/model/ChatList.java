998a7e995ad9928f6b36f30009dc7cc85a41a46a
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2020-06-16 13:37:36 +0100

    Created a layout for chatlist
    created a layout to enable a user view a list of list chat list when combined

---
+++ app/src/main/java/com/example/coat/model/ChatList.java
@@ -0,0 +1,20 @@
+package com.example.coat.model;
+
+public class ChatList {
+    String id; //we wil need this id to get chat list, sender/receiver uid
+
+    public ChatList(String id) {
+        this.id = id;
+    }
+
+    public ChatList() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
